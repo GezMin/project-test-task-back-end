c3fe87344163b573f824db60e4e15dcc6375cc79
GezMin <s.shvetsov.v@gmail.com>
2024-07-04 01:07:37 +0500

    finish

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bull';
+import { CacheModule } from '@nestjs/cache-manager';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './user.entity';
 
 @Module({
-  controllers: [UsersController],
+  imports: [
+    TypeOrmModule.forFeature([User]), // Регистрация сущности User
+    BullModule.registerQueue({
+      name: 'user-status',
+    }),
+    CacheModule.register(),
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
